d005b2e089c993f611c93e04fbb2198f0b2a87c1
USER <ekubmuhamedov@gmail.com>
2021-01-07 17:27:26 +0500

    first commit

---
+++ src/I3/Classes/RoomFare.java
@@ -0,0 +1,28 @@
+package I3.Classes;
+
+/**
+ *
+ * @author Baldezh Team
+ */
+public class RoomFare {
+
+    private String room_type;
+    private int pricePerDay;
+
+    public String getRoom_type() {
+        return room_type;
+    }
+
+    public void setRoom_type(String room_type) {
+        this.room_type = room_type;
+    }
+
+    public int getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(int pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+}
